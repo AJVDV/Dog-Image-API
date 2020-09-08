973d4e62fa36426bf7d6d219b6e76536ccadbdc4
Andrew VanDerVeen <69212799+AJVDV@users.noreply.github.com>
2020-09-08 10:13:53 -0700

    minor changes to functionality

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ function getDogImages(){
     let userNumElement = document.querySelector('#dog-pic-number').value;
     fetch(`https://dog.ceo/api/breeds/image/random/${userNumElement}`)
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => console.log(data))
+    .then(responseJson => displayResults(responseJson))
+    .catch(error => alert('Something has gone wrong. Please refresh, or try again later'));
+    let imageInfo = object.message;
+    let pics = "";
+    for (i=0;i<imageInfo.length;i++){
+        pics += `<li><img src'" + images[i] + "'/><span>" + images[i] + "</span></li>`;
+    }
 /*        let ImageInfo = JSON.parse(data);
         let pics = "";
         for (i=0;i<imagesInfo.length;i++){
@@ -14,18 +21,26 @@ function getDogImages(){
         */
 }
 
-/*function displayResults(responseJson) {
+function displayResults(responseJson) {
     console.log(responseJson);
-    $('ul').append(
-        `<li><img src="${responseJson.message}" class="results-img"></li>`
+/*
+    let imageInfo = JSON.parse(data);
+    let pics = "";
+    for (i=0;i<imageInfo.length;i++){
+        pics += `<li><img src'" + images[i] + "'/><span>" + images[i] + "</span></li>`;
+    }
+*/
+    $('.dog-pics').append(
+        pics
     )
     $('.results').removeClass('hidden');
 }
-*/
+
 function watchForm() {
     $('#js-dog-pic-form').submit(event => {
         event.preventDefault();
         getDogImages();
+        displayResults();
     });
 }
 
